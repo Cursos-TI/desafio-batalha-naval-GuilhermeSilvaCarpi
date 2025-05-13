7709e4b2c7a108be8a4e0ada77d9d565d27264a5
Guilherme Silva Carpi <62569003+GuilhermeSilvaCarpi@users.noreply.github.com>
2025-05-13 00:39:13 +0000

    Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -13,14 +13,18 @@ int main() {
         for (x=0; x<10; x++){
             tabuleiro[y][x] = 0;
         }
-    }
-    // Navil horizontal
+    } // Navil horizontal
     for (x=2; x<5; x++) {
         tabuleiro[8][x] = 3;
-    }
-    // Navil vertical 
-    for (y=2; y<5; y++) {
-        tabuleiro[y][4] = 3;
+    } // Navil vertical 
+    for (y=3; y<6; y++) {
+        tabuleiro[y][2] = 3;
+    } // Navil diagonal principal
+    for (x=7, y=5; x<10; x++, y++) {
+        tabuleiro[y][x] = 3;
+    } // Navil diagonal secundária
+    for (y=1,x=6; y<4; x--,y++) {
+        tabuleiro[y][x] = 3;
     }
 
     // ECREVENDO TABULEIRO
@@ -40,24 +44,19 @@ int main() {
                 printf("%d | ", y);
             // Restante do tabuleiro
             } else {
-                printf("%d ", tabuleiro[y][x]);
-                /*
+                //printf("%d ", tabuleiro[y][x]);
+                //
                 if (tabuleiro[y][x] == 0) {
-                    printf("  ");
+                    printf("- ");
                 } else {
-                    printf("* ");
+                    printf("O ");
                 }
-                */
+                //
             }
         }
         printf("\n");
     }
     printf("\n");
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
